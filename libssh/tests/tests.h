915b3a0255f78a5907d40c07bd197da966596040
Alexander Bergmann <abergmann@suse.com>
2015-11-29 09:33:24 +0000

    Remove libssh and msgpack package forks and use system libraries.

--- libssh/tests/tests.h
+++
@@ -1,8 +0,0 @@
-/*
-This file is distributed in public domain. You can do whatever you want
-with its content.
-*/
-#include <libssh/libssh.h>
-int authenticate (SSH_SESSION *session);
-SSH_OPTIONS *set_opts(int argc, char **argv);
-
