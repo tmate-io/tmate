ba6ac3a363ae7bdd4af946cc81a19f703d06907f
Damian Szymański <damian@assembla.com>
2019-12-06 12:43:04 +0100

    Update paths in build_static_release script (#178)

--- build_static_release.sh
+++ build_static_release.sh
@@ -23,11 +23,11 @@ cd releases
 
 rm -rf $RELEASE_NAME
 mkdir -p $RELEASE_NAME
-docker run --rm local-$PLATFORM/tmate-build cat tmate > $RELEASE_NAME/tmate
+docker run --rm local-$PLATFORM/tmate-build cat /build/tmate > $RELEASE_NAME/tmate
 chmod +x $RELEASE_NAME/tmate
 tar -cf - $RELEASE_NAME | xz > tmate-$VERSION-static-linux-$PLATFORM.tar.xz
 
 rm -rf $RELEASE_NAME-symbols
 mkdir -p $RELEASE_NAME-symbols
-docker run --rm local-$PLATFORM/tmate-build cat tmate.symbols > $RELEASE_NAME-symbols/tmate.symbols
+docker run --rm local-$PLATFORM/tmate-build cat /build/tmate.symbols > $RELEASE_NAME-symbols/tmate.symbols
 tar -cf - $RELEASE_NAME-symbols | xz > dbg-symbols-tmate-$VERSION-static-linux-$PLATFORM.tar.xz
